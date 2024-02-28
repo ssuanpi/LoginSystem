9062b0405d748187616079f39187eefd3f2fc1a8
Juan Comella <juanpablocomella@gmail.com>
2024-02-28 19:54:26 +0100

    Added the username and password

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String username = "username";
+        String password = "password";
+
+        System.out.println("password = " + password);
+    }
+}

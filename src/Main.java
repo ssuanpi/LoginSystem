c3e8c123a7437407d510bd2e1cf1579130c3f6ff
Juan Comella <juanpablocomella@gmail.com>
2024-02-29 18:24:21 +0100

    upadate! added arrays and cancel option

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String username = "username";
-        String password = "1234";
+
+        String[] usernames = {"admin", "username"};
+        String[] passwords = {"9876", "1234"};
+
 
         boolean isAuthenticated = false;
 
@@ -12,14 +14,17 @@ public class Main {
             String uNameIn = JOptionPane.showInputDialog(null, "Enter your username");
             String passIn = JOptionPane.showInputDialog(null, "Enter your password");
 
-            if (username.equals(uNameIn) && password.equals(passIn)) {
-                JOptionPane.showMessageDialog(null, "Welcome \"" + uNameIn + "\"");
-                isAuthenticated = true;
-            } else if (uNameIn == null || passIn == null) {
-                break;
-            } else {
+            for(int i = 0; i < usernames.length ;i++) {
+                if (usernames[i].equals(uNameIn) && passwords[i].equals(passIn)) {
+                    JOptionPane.showMessageDialog(null, "Welcome \"" + uNameIn + "\"");
+                    isAuthenticated = true;
+                    break;
+                } else if (uNameIn == null || passIn == null) {
+                    break;
+                }
+            } if(!isAuthenticated)
                 JOptionPane.showMessageDialog(null, "Password does not match with username.");
-            }
         }
+
     }
 }
